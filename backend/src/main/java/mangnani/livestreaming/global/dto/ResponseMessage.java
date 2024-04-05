c3e98c85a86b1e62949ebc949c949e9bcb9fec93
KzeroJun <rladudwns164@gmail.com>
2024-04-05 18:32:35 +0900

    feat : 모든 응답 메시지 상수클래스 구현

--- backend/src/main/java/mangnani/livestreaming/global/dto/ResponseMessage.java
+++ backend/src/main/java/mangnani/livestreaming/global/dto/ResponseMessage.java
@@ -2,6 +2,15 @@ package mangnani.livestreaming.global.dto;
 
 public interface ResponseMessage {
 
+	//HTTP STATUS 200
 	String SUCCESS = "Success";
+
+	//HTTP STATUS 400
+	String DUPLICATE_LOGIN_ID= "중복된 아이디 입니다.";
+
+	String DUPLICATED_NICKNAME = "중복된 닉네임 입니다.";
+
+	//HTTP STATUS 401
+	String LOGIN_FAILED = "로그인 또는 비밀번호가 올바르지 않습니다";
 
 }
