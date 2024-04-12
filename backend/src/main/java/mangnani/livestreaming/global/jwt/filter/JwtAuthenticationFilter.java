3dd3ca472df621700ee2451a42bc48436e4d6b28
KzeroJun <rladudwns164@gmail.com>
2024-04-12 18:59:38 +0900

    refactor : 토큰 추출 기능 -> 클래스 추출
    -TokenExtractor 클래스로 기능 분리 구현

--- backend/src/main/java/mangnani/livestreaming/global/jwt/filter/JwtAuthenticationFilter.java
+++ backend/src/main/java/mangnani/livestreaming/global/jwt/filter/JwtAuthenticationFilter.java
@@ -7,13 +7,12 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import mangnani.livestreaming.global.exception.NoPermissionTokenException;
+import mangnani.livestreaming.global.jwt.extractor.TokenExtractor;
 import mangnani.livestreaming.global.jwt.provider.JwtTokenProvider;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -24,8 +23,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 	private final JwtTokenProvider jwtTokenProvider;
 	private final RedisTemplate<String, String> redisTemplate;
-	private static final String AUTHORIZATION_HEADER = "Authorization";
-	private static final String BEARER_TYPE = "Bearer ";
+	private final TokenExtractor tokenExtractor;
 	private static final String BLACKLIST = "BL:";
 
 	@Override
@@ -33,7 +31,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 			FilterChain filterChain) throws ServletException, IOException {
 
 		//토큰 추출
-		String token = resolveToken(request);
+		String token = tokenExtractor.resolveAccessToken(request);
 
 		if (token == null) {
 			filterChain.doFilter(request, response);
@@ -54,15 +52,5 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 		}
 
 		filterChain.doFilter(request, response);
-	}
-
-	public String resolveToken(HttpServletRequest request) {
-		String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
-		if (bearerToken != null && StringUtils.hasText(AUTHORIZATION_HEADER)
-				&& bearerToken.startsWith(
-				BEARER_TYPE)) {
-			return bearerToken.substring(7);
-		}
-		return null;
 	}
 }
